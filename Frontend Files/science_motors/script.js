d2719e0b9c7d52b11a3433028b0b5586b4eefe39
shantanu2307 <59186786+shantanu2307@users.noreply.github.com>
2022-01-01 13:51:21 +0530

    Soil and Mineral GUI updated

--- Frontend Files/science_motors/script.js
+++ Frontend Files/science_motors/script.js
@@ -3,13 +3,30 @@ var microscope = document.getElementById("microscope")
 var pump = document.getElementById('pump')
 var test_tube_nozzle = document.getElementById('stepper_nozzle')
 var test_tube_colour = document.getElementById('stepper_colour')
-microscope.onclick = function() {
+var image1 = document.getElementsByClassName('def')[0];
+var image2 = document.getElementsByClassName('red')[0];
+var image3 = document.getElementsByClassName('green')[0];
+var image4 = document.getElementsByClassName('blue')[0];
+var min1 = document.getElementsByClassName('def')[1];
+var min2 = document.getElementsByClassName('red')[1];
+var min3 = document.getElementsByClassName('green')[1];
+var min4 = document.getElementsByClassName('blue')[1];
+
+var sand = document.getElementById('sand');
+var slit = document.getElementById('slit');
+var clay = document.getElementById('clay');
+var finalClassification = document.getElementById('finalSoil')
+var mineralidentified = document.getElementById('mineralIdentified');
+var mineralcolour = document.getElementById('mineralColour');
+var streak = document.getElementById('streak');
+var elements = document.getElementById('elements');
+
+microscope.onclick = function () {
     axios({
         url: 'http://192.168.30.151:8000/capture_micro',
         method: 'GET',
         responseType: 'blob'
-    })
-    .then((response) => {
+    }).then(async (response) => {
         const url = window.URL.createObjectURL(new Blob([response.data]));
         const link = document.createElement('a');
         link.href = url;
@@ -17,8 +34,70 @@ microscope.onclick = function() {
         document.body.appendChild(link);
         link.click();
         document.body.removeChild(link);
-    })
+        image1.src = url;
+        image2.src = url;
+        image3.src = url;
+        image4.src = url;
+    });
+    /* Function for Calling backend */
+    sand.innerHTML = 50;
+    slit.innerHTML = 60;
+    clay.innerHTML = 70;
+    finalClassification.innerHTML = "Red Soil"
+
+
+    //Use for Local Testing
+    // console.log(image1);
+    // console.log(image2);
+    // console.log(image3);
+    // console.log(image4);
+    // const input = document.getElementById('alpha');
+    // const url = window.URL.createObjectURL(new Blob([input.files[0]]));
+    // image1.src = url;
+    // image2.src = url;
+    // image3.src = url;
+    // image4.src = url;
 };
+
+mineral.onclick = function () {
+    axios({
+        url: 'http://192.168.30.151:8000/capture_micro',
+        method: 'GET',
+        responseType: 'blob'
+    }).then(async (response) => {
+        const url = window.URL.createObjectURL(new Blob([response.data]));
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', 'image.jpg');
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+        min1.src = url;
+        min2.src = url;
+        min3.src = url;
+        min4.src = url;
+    });
+    /* Function for Calling backend */
+    mineralcolour.innerHTML = "Brown";
+    mineralidentified.innerHTML = "Aliminium";
+    streak.innerHTML = "Black";
+    elements.innerHTML = "Al, Fe, Xe"
+
+
+    //Use for Local Testing
+    // console.log(image1);
+    // console.log(image2);
+    // console.log(image3);
+    // console.log(image4);
+    // const input = document.getElementById('alpha');
+    // const url = window.URL.createObjectURL(new Blob([input.files[0]]));
+    // image1.src = url;
+    // image2.src = url;
+    // image3.src = url;
+    // image4.src = url;
+};
+
+
 pump.addEventListener('submit', (e) => {
     const formData = new FormData(e.target);
     e.preventDefault();
--- index.js
+++ index.js
@@ -1,8 +1,15 @@
-const {app, BrowserWindow, ipcMain, screen} = require('electron')
+const { app, BrowserWindow, ipcMain, screen } = require('electron')
 const pty = require("node-pty");
 const os = require("os");
 const path = require("path");
 var shell = os.platform() === "win32" ? "powershell.exe" : "bash";
+
+try {
+    require('electron-reloader')(module)
+} catch (_) {
+
+}
+
 
 var width, height, width_sidebar, height_terminal, width_video;
 let mainWindow;
@@ -18,97 +25,98 @@ let control
 let sensor
 let dummy_window
 
-function createDummyWindow(){
+
+function createDummyWindow() {
     dummy_window = new BrowserWindow({
         frame: true,
         webPreferences: {
             devTools: true,
-			contextIsolation: false,
-      		enableRemoteModule: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
             nodeIntegration: true
         }
     });
     dummy_window.loadURL(`file://${__dirname}/dummy.html`);
-    dummy_window.on("closed", function() {
+    dummy_window.on("closed", function () {
         dummy_window = null;
     });
 }
 
-function createMainWindow(){
+function createMainWindow() {
     mainWindow = new BrowserWindow({
-        height: height, 
-        width: width - width_sidebar, 
+        height: height,
+        width: width - width_sidebar,
         frame: false,
-        x: width_sidebar, 
+        x: width_sidebar,
         y: 0,
         webPreferences: {
             devTools: true,
-			contextIsolation: false,
-      		enableRemoteModule: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
             nodeIntegration: true
         }
     });
     mainWindow.loadURL(`file://${__dirname}/Frontend\ Files/landing_page/index.html`);
-    mainWindow.on("closed", function() {
+    mainWindow.on("closed", function () {
         mainWindow = null;
     });
 }
-function createSideBar(){
+function createSideBar() {
     sidebar = new BrowserWindow({
-        height: height, 
-        width: width_sidebar, 
-        x: 0, 
+        height: height,
+        width: width_sidebar,
+        x: 0,
         y: 0,
         frame: false,
         webPreferences: {
             devTools: true,
-			contextIsolation: false,
-      		enableRemoteModule: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
             nodeIntegration: true
         }
     });
     sidebar.loadURL(`file://${__dirname}/Frontend\ Files/SideBar/index.html`);
-    sidebar.on("closed", function() {
+    sidebar.on("closed", function () {
         sidebar = null;
     });
 }
 
-function create_propulsion_speed_window(){
+function create_propulsion_speed_window() {
     propulsion_speed = new BrowserWindow({
-        height: height - height_terminal, 
-        width: (width - width_sidebar) - width_video, 
+        height: height - height_terminal,
+        width: (width - width_sidebar) - width_video,
         show: false,
         frame: false,
         x: width_sidebar + width_video,
         y: 0,
         webPreferences: {
-			contextIsolation: false,
-      		enableRemoteModule: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
             nodeIntegration: true
         }
     });
     propulsion_speed.loadURL(`file://${__dirname}/Frontend\ Files/speedometer/index.html`);
-    propulsion_speed.on("closed", function() {
+    propulsion_speed.on("closed", function () {
         propulsion_speed = null;
     });
 }
-function create_Terminal_propulsion(){
+function create_Terminal_propulsion() {
     prop_terminal = new BrowserWindow({
-        height: height_terminal, 
-        width: (width - width_sidebar) - width_video, 
+        height: height_terminal,
+        width: (width - width_sidebar) - width_video,
         show: false,
         frame: false,
         x: width_sidebar + width_video,
         y: height - height_terminal + 25,
         webPreferences: {
-			contextIsolation: false,
-      		enableRemoteModule: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
             nodeIntegration: true
         }
     });
-    
+
     prop_terminal.loadURL(`file://${__dirname}/propulsion_terminal.html`);
-    prop_terminal.on("closed", function() {
+    prop_terminal.on("closed", function () {
         prop_terminal = null;
     });
 
@@ -120,7 +128,7 @@ function create_Terminal_propulsion(){
         env: process.env
     });
 
-    ptyProcess.on('data', function(data) {
+    ptyProcess.on('data', function (data) {
         prop_terminal.webContents.send("terminal.incomingData", data);
     });
     ptyProcess.write("python3 propulsion.py");
@@ -128,40 +136,40 @@ function create_Terminal_propulsion(){
         ptyProcess.write(key);
     });
 }
-function createVideoWindow(){
-	video_window = new BrowserWindow({
-		height: height, 
-		width: width_video, 
+function createVideoWindow() {
+    video_window = new BrowserWindow({
+        height: height,
+        width: width_video,
         frame: false,
         x: width_sidebar,
         y: 0,
-		webPreferences: {
-			nodeIntegration: true
-		}
-	});
-	video_window.loadURL(`file://${__dirname}/Frontend\ Files/video_stream/index.html`);
-	video_window.on("closed", function() {
-		video_window = null;
-	});
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    video_window.loadURL(`file://${__dirname}/Frontend\ Files/video_stream/index.html`);
+    video_window.on("closed", function () {
+        video_window = null;
+    });
 }
 
-function create_Terminal_arm(){
+function create_Terminal_arm() {
     arm_terminal = new BrowserWindow({
-        height: height_terminal, 
-        width: (width - width_sidebar) - width_video, 
+        height: height_terminal,
+        width: (width - width_sidebar) - width_video,
         show: false,
         frame: false,
         x: width_sidebar + width_video,
         y: height - height_terminal + 25,
         webPreferences: {
-			contextIsolation: false,
-      		enableRemoteModule: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
             nodeIntegration: true
         }
     });
-    
+
     arm_terminal.loadURL(`file://${__dirname}/arm_terminal.html`);
-    arm_terminal.on("closed", function() {
+    arm_terminal.on("closed", function () {
         arm_terminal = null;
     });
 
@@ -173,7 +181,7 @@ function create_Terminal_arm(){
         env: process.env
     });
 
-    ptyProcess.on('data', function(data) {
+    ptyProcess.on('data', function (data) {
         arm_terminal.webContents.send("terminal.incomingData", data);
     });
     ptyProcess.write("python3 /Users/harshgupta/Desktop/Ares/Application/arm.py");
@@ -182,104 +190,104 @@ function create_Terminal_arm(){
     });
 }
 
-function create_arm_speed_window(){
+function create_arm_speed_window() {
     arm_speed = new BrowserWindow({
-        height: height - height_terminal, 
-        width: (width - width_sidebar) - width_video, 
+        height: height - height_terminal,
+        width: (width - width_sidebar) - width_video,
         show: false,
         frame: false,
         x: width_sidebar + width_video,
         y: 0,
         webPreferences: {
-			contextIsolation: false,
-      		enableRemoteModule: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
             nodeIntegration: true
         }
     });
     arm_speed.loadURL(`file://${__dirname}/Frontend\ Files/arm_motors/index.html`);
-    arm_speed.on("closed", function() {
+    arm_speed.on("closed", function () {
         arm_speed = null;
     });
 }
 
-function create_science_motor_window(){
+function create_science_motor_window() {
     science_motors = new BrowserWindow({
-        height: height, 
-        width: (width - width_sidebar) - width_video, 
+        height: height,
+        width: (width - width_sidebar) - width_video,
         show: false,
         frame: false,
         x: width_sidebar + width_video,
         y: 0,
         webPreferences: {
-			contextIsolation: false,
-      		enableRemoteModule: true,
-			nodeIntegration: true
+            contextIsolation: false,
+            enableRemoteModule: true,
+            nodeIntegration: true
         }
     });
     science_motors.loadURL(`file://${__dirname}/Frontend\ Files/science_motors/index.html`);
-    science_motors.on("closed", function() {
+    science_motors.on("closed", function () {
         science_motors = null;
     });
 }
 
-function createVideoWindow_science(){
-	video_window_science = new BrowserWindow({
-		height: height, 
-		width: width_video, 
+function createVideoWindow_science() {
+    video_window_science = new BrowserWindow({
+        height: height,
+        width: width_video,
         frame: false,
         x: width_sidebar,
         y: 0,
         show: false,
-		webPreferences: {
-			nodeIntegration: true
-		}
-	});
-	video_window_science.loadURL(`file://${__dirname}/Frontend\ Files/video_stream_science/index.html`);
-	video_window_science.on("closed", function() {
-		video_window_science = null;
-	});
+        webPreferences: {
+            nodeIntegration: true
+        }
+    });
+    video_window_science.loadURL(`file://${__dirname}/Frontend\ Files/video_stream_science/index.html`);
+    video_window_science.on("closed", function () {
+        video_window_science = null;
+    });
 }
 
-function create_control_window(){
-	control = new BrowserWindow({
-		height: height, 
-        width: width - width_sidebar, 
+function create_control_window() {
+    control = new BrowserWindow({
+        height: height,
+        width: width - width_sidebar,
         frame: false,
-        x: width_sidebar, 
+        x: width_sidebar,
         y: 0,
         show: false,
-		webPreferences: {
+        webPreferences: {
             contextIsolation: false,
-      		enableRemoteModule: true,
-			nodeIntegration: true
-		}
-	});
-	control.loadURL(`file://${__dirname}/Frontend\ Files/Control/index.html`);
-	control.on("closed", function() {
-		control = null;
-	});
+            enableRemoteModule: true,
+            nodeIntegration: true
+        }
+    });
+    control.loadURL(`file://${__dirname}/Frontend\ Files/Control/index.html`);
+    control.on("closed", function () {
+        control = null;
+    });
 };
 
-function create_sensor_window(){
+function create_sensor_window() {
     sensor = new BrowserWindow({
-        height: height, 
-        width: width - width_sidebar, 
+        height: height,
+        width: width - width_sidebar,
         frame: false,
-        x: width_sidebar, 
+        x: width_sidebar,
         y: 0,
         webPreferences: {
-			contextIsolation: false,
-      		enableRemoteModule: true,
+            contextIsolation: false,
+            enableRemoteModule: true,
             nodeIntegration: true
         }
     });
     sensor.loadURL(`file://${__dirname}/Frontend\ Files/Sensors/index.html`);
-    sensor.on("closed", function() {
+    sensor.on("closed", function () {
         sensor = null;
     });
 }
 
-app.on("ready", function() {
+app.on("ready", function () {
     createDummyWindow()
     dummy_window.maximize();
     width = screen.getPrimaryDisplay().workAreaSize.width;
@@ -292,23 +300,23 @@ app.on("ready", function() {
     createSideBar();
     dummy_window.close();
     sidebar.setAlwaysOnTop(true);
-    ipcMain.on('open-propulsion', function(){
+    ipcMain.on('open-propulsion', function () {
 
-        if (propulsion_speed == null) 
+        if (propulsion_speed == null)
             create_propulsion_speed_window();
         propulsion_speed.show();
 
-       if (prop_terminal == null)
+        if (prop_terminal == null)
             create_Terminal_propulsion();
         prop_terminal.show();
-        
+
         if (video_window == null)
             createVideoWindow()
         video_window.show();
 
     });
-    
-    ipcMain.on('return_main', function() {
+
+    ipcMain.on('return_main', function () {
         if (mainWindow == null)
             createMainWindow();
         mainWindow.show()
@@ -318,40 +326,40 @@ app.on("ready", function() {
         sidebar.show()
     });
 
-    ipcMain.on('open-arm', function(){
+    ipcMain.on('open-arm', function () {
 
-        if (arm_speed == null) 
+        if (arm_speed == null)
             create_arm_speed_window();
         arm_speed.show();
 
         if (arm_terminal == null)
             create_Terminal_arm();
         arm_terminal.show();
-        
+
         if (video_window == null)
             createVideoWindow()
         video_window.show();
     });
 
-    ipcMain.on('open-science', function(){
+    ipcMain.on('open-science', function () {
 
-        if (science_motors == null) 
+        if (science_motors == null)
             create_science_motor_window();
         science_motors.show();
-        
+
         if (video_window_science == null)
             createVideoWindow_science()
         video_window_science.show();
     });
 
-    ipcMain.on('open-control', function(){
+    ipcMain.on('open-control', function () {
 
         if (control == null)
             create_control_window()
         control.show();
     });
 
-    ipcMain.on('open-sensor', function(){
+    ipcMain.on('open-sensor', function () {
 
         if (sensor == null)
             create_sensor_window()
